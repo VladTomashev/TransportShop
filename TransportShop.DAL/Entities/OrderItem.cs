2d62358c7e87e48df10e40bd91b17c4ce8072527
VladTomashev <tomashev.vladislav@gmail.com>
2024-07-09 17:10:30 +0300

    added entities

---
+++ TransportShop.DAL/Entities/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TransportShopAPI.Entities
+{
+    public class OrderItem : AbstractEntity
+    {
+        public int idProduct { get; set; }
+        public int idOrders { get; set; }
+        public int count { get; set; }
+
+        public Product product { get; set; }
+        public Order order { get; set; }
+    }
+}
---
+++ TransportShop.DAL/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TransportShopAPI.Entities
+{
+    public class User : AbstractEntity
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+
+        public List<Order> orders { get; set; }
+    }
+}
