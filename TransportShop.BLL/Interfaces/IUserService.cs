e671999bf9739caca6a1ee843e959b79da2fb8be
Baldj <mihal275@mail.ru>
2024-07-10 20:49:49 +0300

    Created IUserService interface

---
+++ TransportShop.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using TransportShop.DAL.Entities;
+
+namespace TransportShop.BLL.Interfaces
+{
+    internal interface IUserService : IService<User>
+    {
+        public User GetUserByOrder(int orderId);
+    }
+}
