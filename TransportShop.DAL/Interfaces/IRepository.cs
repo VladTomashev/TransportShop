1427625b6a354dbf40009144af73a4ca0b57890e
VladTomashev <tomashev.vladislav@gmail.com>
2024-07-10 15:20:32 +0300

    added base repository

---
+++ TransportShop.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using TransportShop.DAL.Entities;
+
+namespace TransportShop.DAL.Interfaces
+{
+    internal interface IRepository<T> where T : AbstractEntity
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
---
+++ TransportShop.DAL/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿using TransportShop.DAL.EF;
+using TransportShop.DAL.Entities;
+using TransportShop.DAL.Interfaces;
+
+namespace TransportShop.DAL.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : AbstractEntity
+    {
+        protected DataContext db;
+        public Repository()
+        {
+            db = new DataContext();
+        }
+
+        public void Add(T entity)
+        {
+            db.Set<T>().Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            T entity = db.Set<T>().Find(id);
+
+            if (entity != null)
+            {
+                db.Set<T>().Remove(entity);
+                db.SaveChanges();
+            }
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return db.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            db.Set<T>().Update(entity);
+            db.SaveChanges();
+        }
+    }
+}
