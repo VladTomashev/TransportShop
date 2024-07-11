7e958a9e5455b0f7788c778bf191d126b3eceb1f
Ира Куцаева <ira.kutsaeva@gmail.com>
2024-07-11 16:02:59 +0300

    Создан сервис OrderItems

---
+++ TransportShop.BLL/Interfaces/IService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TransportShop.DAL.Entities;
+
+namespace TransportShop.BLL.Interfaces
+{
+    internal interface IService<T> where T : AbstractEntity
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Delete(int id);
+    }
+}
