b406899de6ab2706f86d0c36e70f24640deb1eac
Anna Petrova <anna5437743@mail.ru>
2024-07-11 12:02:08 +0300

    added dto models for orders and interfase for orderservice

---
+++ TransportShop.BLL/DTO/OrderResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace TransportShop.BLL.DTO
+{
+    internal class OrderResponse
+{
+    public double TotalPrice { get; set; }
+    public DateTime OrderDate { get; set; }
+}
+}
---
+++ TransportShop.BLL/Interfaces/IService.cs
@@ -0,0 +1,13 @@
+﻿using TransportShop.DAL.Entities;
+
+namespace TransportShop.BLL.Interfaces
+{
+    internal interface IService<T> where T : AbstractEntity
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
