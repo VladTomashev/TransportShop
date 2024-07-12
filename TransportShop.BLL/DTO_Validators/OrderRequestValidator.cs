af1e0b30e0ab42168347805363e5ab4f3748d4e3
Anna Petrova <anna5437743@mail.ru>
2024-07-12 11:12:52 +0300

    added validation

---
+++ TransportShop.BLL/DTO_Validators/OrderRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using TransportShop.BLL.DTO;
+
+namespace TransportShop.BLL.DTO_Validators
+{
+    internal class OrderRequestValidator : AbstractValidator<OrderRequest>
+    {
+        public OrderRequestValidator()
+        {
+            RuleFor(order => order.UserName)
+                    .NotEmpty()
+                    .MinimumLength(4)
+                    .WithMessage("Username must contain more than 3 characters");
+
+            RuleFor(order => order.UserName)
+                    .Matches(@"^[A-Za-z0-9_]*$")
+                    .WithMessage("Username must contain only letters, numbers or underline");
+
+            RuleFor(order => order.Items)
+                .NotNull()
+                .WithMessage("There are no items");
+        }
+    }
+}
