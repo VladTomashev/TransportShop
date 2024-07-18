78912428a3742f4f73e0c04132d7e40e91aed40c
Baldj <mihal275@mail.ru>
2024-07-18 17:10:57 +0300

    Added OrderListResponse and changed corresponding logic in OrderService

---
+++ TransportShop.BLL/DTO/Response/OrderListResponse.cs
@@ -0,0 +1,9 @@
+﻿using TransportShop.DAL.Entities;
+
+namespace TransportShop.BLL.DTO.Response
+{
+    public class OrdersListResponse
+    {
+        public List<Order> Orders { get; set; }
+    }
+}
